290a9b17eaa8375c7209a75ba70efc45d8de7343
gabriellalmeida <almeidagabriel016@gmail.com>
2022-03-19 16:56:20 -0300

    Primeiro commit com cami-la

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		
+		/*int a = 10;
+		int b = 6;
+		
+		System.out.println("Hello World! " + (a+b));*/
+
+	}
+
+}
